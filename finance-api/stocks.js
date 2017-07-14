4a0ca96a852f45c9a409e122e26805734c674874
Josenivaldo Benito Jr <jrbenito@benito.qsl.br>
2017-07-14 16:30:37 -0300

    Stocks - Create event like JCP, Dividendos with proper value
    Signed-off-by: Josenivaldo Benito Jr <jrbenito@benito.qsl.br>

--- finance-api/stocks.js
+++ finance-api/stocks.js
@@ -1,16 +1,47 @@
 'use strict';
 
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const ddb = new AWS.DynamoDB.DocumentClient();
+var dbParams = {
+    TableName: process.env.DYNAMODB_TABLE_STOCK_EV,
+
+}
+
 module.exports.createEvent = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
 
-  callback(null, response);
+    const timestamp = new Date().getTime();
+    var data = JSON.parse(event.body);
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    if (typeof data.id !== 'string') {
+        console.error('Validation Error: ID is not string');
+        callback(new Error('Validation error: ID must be string');
+        return;
+    }
+    if (typeof data.eventDate !== 'string') {
+        console.error('Validation Error: set date to now');
+        data.eventDate = timestamp;
+    }
+
+    data.createdAt = timestamp;
+    data.updatedAt = timestamp;
+
+    dbParams.Item = data;
+
+    // write to to table
+    ddb.put(dbParams, (error) => {
+        // Error?
+        if (error) {
+            console.error(error);
+            callback(new Error('Couldn\´t create event'));
+            return;
+        }
+
+        // no error, let´s respond ok
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(dbParams.Item),
+        };
+        callback(null, response);
+    });
 };
